a3906de3e48860d8281a8d02bca29cb26e8b78cb
parciffal <khachikyan.edgar@gmail.com>
2023-02-12 22:24:22 +0400

    df

--- src/Homework5/shop/store/admin.py
+++ src/Homework5/shop/store/admin.py
@@ -3,22 +3,14 @@ from django.contrib import admin
 from .models import *
 
 
-#admin.site.register(ItemCategory)
-#admin.site.register(Customer)
-#admin.site.register(MyBug)
 
-#admin.site.register(StoreOwner)
-#admin.site.register(StoreCategory)
-#admin.site.register(Store)
-
-#admin.site.register(Purchase)
 @admin.register(StoreCategory)
 class StoreCategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'picture']
 
 @admin.register(StoreOwner)
 class StoreOwnerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'avatar', 'registrated_at']
+    list_display = ['user', 'avatar', 'registrated_at']
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
@@ -39,17 +31,21 @@ class ItemAdmin(admin.ModelAdmin):
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'avatar', 'registrated_at']
+    list_display = ['user', 'avatar', 'registrated_at']
 
 
 @admin.register(MyBug)
 class MyBugAdmin(admin.ModelAdmin):
-    list_display = ['custumer', 'total_price']
+    list_display = ['custumer', 'total_price', 'items']
+    list_display_links = ['items']
 
+    def items(self):
+        return self.items.name
 
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
-    list_display = ['total_price', 'buy_time', 'custumer', 'get_items']
+    list_display = ['total_price', 'buy_time', 'custumer', 'items']
+    list_display_links = ['items']
 
-    def get_items(self):
-        return [item for item in self.items.all()]
+    def items(self):
+        return self.items.name
